cbe029c204aaac639ba0328d1637270b764d79b3
marcusmolchany <marcusmolchany@gmail.com>
2013-11-10 21:46:25 -0500

    Answer for Euler Problem 1.

--- problem_1.py
+++ problem_1.py
@@ -1,1 +1,11 @@
 # problem 1
+print "Euler Problem 1"
+
+sum = 0
+
+for number in range (1, 100):
+  if not (number % 3 and number % 5):
+    print number , " "
+    sum += number
+
+print "\nsum is ", sum
